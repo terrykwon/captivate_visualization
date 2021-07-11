6a8ec0143b6692fb7007715d3f28d313b20a6e7f
Terry Kwon <terrykkwon@gmail.com>
2021-07-11 11:58:37 +0900

    Add files

---
+++ state.py
@@ -0,0 +1,96 @@
+import math
+
+
+NUM_CARDS = 6
+
+TOYS = ['공', '버스', '꽃', '강아지', '고양이', '신발', 
+        '숟가락', '물고기', '자전거', '곰돌이', '아기', '가방']
+
+NUM_TOYS = len(TOYS)
+
+
+class Toy:
+    def __init__(self, name):
+        self.name = name
+        self.weight = 1 / NUM_TOYS
+        self.phrases = [
+            Phrase('{}_{}'.format(name, i), self) for i in range(1, 7)
+        ]
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+
+class Phrase:
+    def __init__(self, phrase, toy, target_word=None, highlight=None):
+        self.phrase = phrase
+        self.toy = toy
+        self.target_word = target_word
+        self.highlight = highlight
+
+
+class Context:
+    def __init__(self):
+        self.toys = [Toy(name) for name in TOYS]
+
+
+    def on_visual_attention(self, toy_name):
+        ALPHA = 0.02
+
+        for toy in self.toys:
+            if toy.name == toy_name:
+                toy.weight = toy.weight + ALPHA
+        
+            toy.weight /= (1 + ALPHA)  # normalize
+
+        self.toys = sorted(self.toys, reverse=True)
+
+
+    def on_spoken_attention(self, attended_toy):
+        BETA = 0.2
+        for toy in self.toys:
+            if toy.name == attended_toy:
+                toy.weight += BETA
+            else:
+                toy.weight -= BETA
+        self.toys = sorted(self.toys, reverse=True)
+
+
+class State:
+    ''' The state is a function of
+            (1) the joint attention distribution (context)
+            (2) the spoken target words
+            (3) time
+    '''
+    def __init__(self):
+        self.context = Context()
+        self.time = 0
+
+    
+    def on_time_step_increment(self):
+        self.time += 1
+
+
+    def on_target_word_spoken(self, target_word):
+        pass
+
+
+    def context_to_guidance(self):
+        cards = []
+        for toy in self.context.toys:
+            how_many = math.ceil(toy.weight * NUM_CARDS)
+            cards.extend(toy.phrases[:how_many])
+            if len(cards) >= NUM_CARDS:
+                break
+
+        cards = cards[:NUM_CARDS]  # truncate
+
+        print('----------------')
+        for phrase in cards:
+            print(phrase.phrase)
+        print('----------------')
+        for toy in self.context.toys:
+            print(f'{toy.name:5}: weight = {toy.weight:.3f}  | ', end='')
+            print(', '.join([x.phrase for x in toy.phrases]))
+
+
